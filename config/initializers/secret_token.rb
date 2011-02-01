fa16d4230cf90119de5e1837680ac5c3b6c29d4d
Omar Paro <omar.paro@googlemail.com>
2011-02-01 23:01:13 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialNetworking::Application.config.secret_token = '31aa999c5cb52d9300926681303b14728313ba6bd9e54570bb8430f0ff00ae0b66739df4b3a5d0f402986a814b5a58ed65bf0bf9204e8750ad513c43492fced8'
